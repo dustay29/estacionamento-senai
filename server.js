d724b38f7223ef1da162afc0595d55acf1880107
xandale <ale_guitis@hotmail.com>
2025-05-28 21:16:09 -0300

    first commit

---
+++ server.js
@@ -0,0 +1,25 @@
+import express from 'express';
+import 'dotenv/config'
+import cors from 'cors'
+// importando  express em uma variavel
+const app = express();
+//autorizando uso pelo corns
+app.use(cors());
+// Importar a conexao criada com sequelize
+// import {database} from './database.js'
+// import { router } from './routes/routes.js'
+// ultilizando json
+app.use(express.json());
+// futuro uso do router app.use(router)
+
+try{
+   // await Tarefa.sync({ alter: true })
+    // await Heroi.sync({ force: true })
+} catch(erro){
+    console.log(erro)
+}
+
+const PORT = process.env.PORT;
+app.listen(PORT, () => {
+    console.log(`🚀 Servidor rodando em http://localhost:${PORT}`)
+})
