8f3a66a727912c3e2352d8cb0600df20fcadf801
dustay29 <dustaydustay@gmail.com>
2025-06-04 20:00:24 -0300

    add de pasta garai

--- database.js
+++ database.js
@@ -4,7 +4,6 @@ import { Sequelize } from "sequelize";
 const database = new Sequelize(process.env.BANCO_DE_DADOS)
 
 try{
-        // Testar a conexão com o banco de dados
     await database.authenticate()
       console.log('Banco de dados conectado com sucesso')
 } catch(erro) {
