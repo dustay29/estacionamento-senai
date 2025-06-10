54b9ea4bcaedd482e2a3a38877e87a0ea638bca6
xandale <ale_guitis@hotmail.com>
2025-06-10 10:14:54 -0300

    teste de conecção com o banco de dados correção de erros no acesso comcluida

--- server.js
+++ server.js
@@ -6,15 +6,19 @@ const app = express();
 //autorizando uso pelo corns
 app.use(cors());
 // Importar a conexao criada com sequelize
-// import {database} from './database.js'
+import {database} from './database.js'
+import { Veiculos } from './models/veiculo.js';
+import { Acesso } from './models/acesso.js';
+import { Usuarios } from './models/usuario.js';
 // import { router } from './routes/routes.js'
 // ultilizando json
 app.use(express.json());
 // futuro uso do router app.use(router)
 
 try{
-   // await Tarefa.sync({ alter: true })
-    // await Heroi.sync({ force: true })
+   await Usuarios.sync({ alter: true }); // ⬅️ primeiro
+    await Veiculos.sync({ alter: true }); // ⬅️ depois
+    await Acesso.sync({ alter: true });   // ⬅️ por último
 } catch(erro){
     console.log(erro)
 }
